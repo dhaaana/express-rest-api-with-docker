7168a5dad5ed1a0fbd6d878ed6889b70e2edec34
dhana <ddhanarr@gmail.com>
2022-09-17 22:34:04 +0700

    feat: initialize prisma and docker postgres

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-console.log("hello world");
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+import config from "config";
+
+const prisma = new PrismaClient();
+const port = config.get<number>("port");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "halooo" });
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  if (users.length === 0) {
+    res.json({ message: "no users" });
+    return;
+  }
+  res.json();
+});
+
+app.listen(port, () => {
+  console.log(`app is running on port ${port}`);
+});
